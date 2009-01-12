a9f2b6f5f74b71db2044ded02e43777586ce135d
Yaron Koren <yaron@users.mediawiki.org>
2009-01-12 15:50:45 +0000

    New extension

---
+++ ED_ParserFunctions.php
@@ -0,0 +1,100 @@
+<?php
+ 
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'This file is a MediaWiki extension; it is not a valid entry point' );
+}
+
+/**
+ * Class for handling the parser functions for External Data
+ *
+ * @author Yaron Koren
+ */
+class EDParserFunctions {
+
+	// XML-handling functions based on code found at
+	// http://us.php.net/xml_set_element_handler
+	static function startElement( $parser, $name, $attrs ) {
+		global $edgCurrentXMLTag;
+		// set to all lowercase to avoid casing issues
+		$edgCurrentXMLTag = strtolower($name);
+	}
+
+	static function endElement( $parser, $name ) {
+		global $edgCurrentXMLTag;
+		$edgCurrentXMLTag = "";
+	}
+
+	static function getContent ( $parser, $content ) {
+		global $edgCurrentXMLTag, $edgXMLValues;
+		$edgXMLValues[$edgCurrentXMLTag] = $content;
+	}
+
+	static function getXMLData ( $xml ) {
+		global $edgXMLValues;
+		$edgXMLValues = array();
+
+		$xml_parser = xml_parser_create();
+		xml_set_element_handler( $xml_parser, "EDParserFunctions::startElement", "EDParserFunctions::endElement" );
+		xml_set_character_data_handler( $xml_parser, "EDParserFunctions::getContent" );
+		if (!xml_parse($xml_parser, $xml, true)) {
+			die(sprintf("XML error: %s at line %d",
+			xml_error_string(xml_get_error_code($xml_parser)),
+			xml_get_current_line_number($xml_parser)));
+		}
+		xml_parser_free( $xml_parser );
+		return $edgXMLValues;
+	}
+
+	static function getCSVData ( $csv ) {
+		// regular expression copied from http://us.php.net/fgetcsv
+		$csv_vals = preg_split('/,(?=(?:[^\"]*\"[^\"]*\")*(?![^\"]*\"))/', $csv); 
+		// start with a null value so that the real values start with
+		// an index of 1 instead of 0
+		$values = array(null);
+		foreach ( $csv_vals as $csv_val ) {
+			$values[] = trim( $csv_val, '"' );
+		}
+		return $values;
+	}
+ 
+	/**
+	 * Render the #get_external_data parser function
+	 */
+	static function doGetExternalData( &$parser ) {
+		global $edgValues;
+		$params = func_get_args();
+		array_shift( $params ); // we already know the $parser ...
+		$url = array_shift( $params );
+		$url_contents = file_get_contents( $url );
+		$format = array_shift( $params );
+		$external_values = array();
+		if ($format == 'xml') {
+			$external_values = self::getXMLData( $url_contents );
+		} elseif ($format == 'csv') {
+			$external_values = self::getCSVData( $url_contents );
+		}
+		// for each external variable name specified in the function
+		// call, get its value (if one exists), and attach it to the
+		// local variable name
+		foreach ($params as $param) {
+			list( $local_var, $external_var ) = explode( '=', $param );
+			// set to all lowercase to avoid casing issues
+			$external_var = strtolower( $external_var );
+			if ( array_key_exists( $external_var, $external_values ) )
+				$edgValues[$local_var] = $external_values[$external_var];
+		}
+
+		return '';
+	}
+ 
+	/**
+	 * Render the #external_value parser function
+	 */
+	static function doExternalValue( &$parser, $local_var = '' ) {
+		global $edgValues;
+		if ( array_key_exists( $local_var, $edgValues) )
+			return $edgValues[$local_var];
+		else
+			return '';
+	}
+}
---
+++ ED_Settings.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Initialization file for the External Data extension
+ *
+ * @file
+ * @ingroup ExternalData
+ * @author Yaron Koren
+ */
+
+if (!defined('MEDIAWIKI')) die();
+
+$wgExtensionCredits['parserhook'][]= array(
+	'name'	=> 'External Data',
+	'version'	=> '0.1',
+	'author'	=> 'Yaron Koren',
+	'url'	=> 'http://www.mediawiki.org/wiki/Extension:External_Data',
+	'description'	=>  'Allows creating variables from an external XML or CSV file',
+);
+
+$wgExtensionFunctions[] = 'edgParserFunctions';
+$wgHooks['LanguageGetMagic'][] = 'edgLanguageGetMagic';
+
+$edgIP = $IP . '/extensions/ExternalData';
+$wgAutoloadClasses['EDParserFunctions'] = $edgIP . '/ED_ParserFunctions.php';
+
+$edgValues = array();
+
+function edgParserFunctions() {
+	global $wgHooks, $wgParser;
+	if( defined( 'MW_SUPPORTS_PARSERFIRSTCALLINIT' ) ) {
+		$wgHooks['ParserFirstCallInit'][] = 'edgRegisterParser';
+	} else {
+		if ( class_exists( 'StubObject' ) && !StubObject::isRealObject( $wgParser ) ) {
+			$wgParser->_unstub();
+		}
+		edgRegisterParser( $wgParser );
+	}
+}
+
+function edgRegisterParser(&$parser) {
+	$parser->setFunctionHook( 'get_external_data', array('EDParserFunctions','doGetExternalData') );
+	$parser->setFunctionHook( 'external_value', array('EDParserFunctions','doExternalValue') );
+	return true; // always return true, in order not to stop MW's hook processing!
+}
+
+function edgLanguageGetMagic( &$magicWords, $langCode = "en" ) {
+	switch ( $langCode ) {
+	default:
+		$magicWords['get_external_data'] = array ( 0, 'get_external_data' );
+		$magicWords['external_value'] = array ( 0, 'external_value' );
+	}
+	return true;
+}
