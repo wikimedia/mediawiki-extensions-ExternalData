74793a6050bfdf9506ef3c7f3566a172a0d39505
Yaron Koren <yaron@users.mediawiki.org>
2009-02-10 18:27:36 +0000

    New internationalization file

---
+++ ExternalData.i18n.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Internationalization file for the External Data extension
+ *
+ * @addtogroup Extensions
+*/
+
+$messages = array();
+
+/** English
+ * @author Yaron Koren
+ */
+$messages['en'] = array(
+	// user messages
+	'getdata' => 'Get data',
+	'externaldata-desc' => 'Allows for retrieving data in CSV, JSON and XML formats from both external URLs and local wiki pages',
+);
