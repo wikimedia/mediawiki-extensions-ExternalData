915694b756847ae903a1201b076408178e2a6d22
Yaron Koren <yaron@users.mediawiki.org>
2009-02-10 18:44:55 +0000

    Added handling for 'GetData' page and #for_external_table function

--- ED_Settings.php
+++ ED_Settings.php
@@ -11,17 +11,22 @@ if (!defined('MEDIAWIKI')) die();
 
 $wgExtensionCredits['parserhook'][]= array(
 	'name'	=> 'External Data',
-	'version'	=> '0.3',
+	'version'	=> '0.4',
 	'author'	=> array('Yaron Koren', 'Michael Dale'),
 	'url'	=> 'http://www.mediawiki.org/wiki/Extension:External_Data',
-	'description'	=>  'Allows creating variables from an external XML, CSV or JSON file',
+	'description'	=>  'Allows for retrieving data in CSV, JSON and XML formats from both external URLs and local wiki pages',
 );
+$edgIP = $IP . '/extensions/ExternalData';
 
 $wgExtensionFunctions[] = 'edgParserFunctions';
+$wgExtensionMessagesFiles['ExternalData'] = $edgIP . '/ExternalData.i18n.php';
 $wgHooks['LanguageGetMagic'][] = 'edgLanguageGetMagic';
 
-$edgIP = $IP . '/extensions/ExternalData';
+// register all special pages and other classes
+$wgAutoloadClasses['EDUtils'] = $edgIP . '/ED_Utils.php';
 $wgAutoloadClasses['EDParserFunctions'] = $edgIP . '/ED_ParserFunctions.php';
+$wgSpecialPages['GetData'] = 'EDGetData';
+$wgAutoloadClasses['EDGetData'] = $edgIP . '/ED_GetData.php';
 
 $edgValues = array();
 $edgStringReplacements = array();
@@ -42,6 +47,8 @@ function edgParserFunctions() {
 function edgRegisterParser(&$parser) {
 	$parser->setFunctionHook( 'get_external_data', array('EDParserFunctions','doGetExternalData') );
 	$parser->setFunctionHook( 'external_value', array('EDParserFunctions','doExternalValue') );
+	$parser->setFunctionHook( 'for_external_table', array('EDParserFunctions','doForExternalTable') );
+
 	return true; // always return true, in order not to stop MW's hook processing!
 }
 
@@ -50,6 +57,7 @@ function edgLanguageGetMagic( &$magicWords, $langCode = "en" ) {
 	default:
 		$magicWords['get_external_data'] = array ( 0, 'get_external_data' );
 		$magicWords['external_value'] = array ( 0, 'external_value' );
+		$magicWords['for_external_table'] = array ( 0, 'for_external_table' );
 	}
 	return true;
 }
