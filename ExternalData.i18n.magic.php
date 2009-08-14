da33c6eb75400e108f0b20a84d8f23dfb38c5cb5
Siebrand Mazeland <siebrand@users.mediawiki.org>
2009-08-14 14:19:47 +0000

    Localisation updates for extension magic words from translatewiki.net

--- ExternalData.i18n.magic.php
+++ ExternalData.i18n.magic.php
@@ -7,5 +7,18 @@ $magicWords['en'] = array(
 	'get_ldap_data'      => array ( 0, 'get_ldap_data' ),
 	'get_db_data'        => array ( 0, 'get_db_data' ),
 	'external_value'     => array ( 0, 'external_value' ),
-	'for_external_table' => array ( 0, 'for_external_table' )
+	'for_external_table' => array ( 0, 'for_external_table' ),
+);
+
+$magicWords['ar'] = array(
+	'external_value'       => array( '0', 'قيمة_خارجية', 'external_value' ),
+	'for_external_table'   => array( '0', 'لجدول_خارجي', 'for_external_table' ),
+);
+
+$magicWords['nl'] = array(
+	'get_external_data'    => array( '0', 'externe_gegevens_ophalen', 'get_external_data' ),
+	'get_ldap_data'        => array( '0', 'ldap_gegevens_ophalen', 'get_ldap_data' ),
+	'get_db_data'          => array( '0', 'db_gegevens_ophalen', 'get_db_data' ),
+	'external_value'       => array( '0', 'externe_waarde', 'external_value' ),
+	'for_external_table'   => array( '0', 'voor_externe_tabel', 'for_external_table' ),
 );
