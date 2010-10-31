dedf28bc07e618105e8ce467d0311907b3bbda66
Siebrand Mazeland <siebrand@users.mediawiki.org>
2010-10-31 17:55:47 +0000

    Localisation updates from translatewiki.net

--- ExternalData.i18n.magic.php
+++ ExternalData.i18n.magic.php
@@ -2,80 +2,100 @@
 
 $magicWords = array();
 
+/** English (English) */
 $magicWords['en'] = array(
-	'get_external_data'  => array ( 0, 'get_external_data' ),
-	'get_web_data'       => array ( 0, 'get_web_data' ),
-	'get_ldap_data'      => array ( 0, 'get_ldap_data' ),
-	'get_db_data'        => array ( 0, 'get_db_data' ),
-	'external_value'     => array ( 0, 'external_value' ),
-	'for_external_table' => array ( 0, 'for_external_table' ),
-	'store_external_table' => array ( 0, 'store_external_table' ),
+	'get_external_data' => array( 0, 'get_external_data' ),
+	'get_web_data' => array( 0, 'get_web_data' ),
+	'get_ldap_data' => array( 0, 'get_ldap_data' ),
+	'get_db_data' => array( 0, 'get_db_data' ),
+	'external_value' => array( 0, 'external_value' ),
+	'for_external_table' => array( 0, 'for_external_table' ),
+	'store_external_table' => array( 0, 'store_external_table' ),
 );
 
+/** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'get_external_data'      => array( '0', 'عرض_بيانات_خارجية', 'get_external_data' ),
-	'get_ldap_data'          => array( '0', 'عرض_بيانات_لداب', 'get_ldap_data' ),
-	'get_db_data'            => array( '0', 'عرض_بيانات_قب', 'get_db_data' ),
-	'external_value'         => array( '0', 'قيمة_خارجية', 'external_value' ),
-	'for_external_table'     => array( '0', 'لجدول_خارجي', 'for_external_table' ),
-	'store_external_table'   => array( '0', 'تخزين_جدول_خارجي', 'store_external_table' ),
+	'get_external_data' => array( 0, 'عرض_بيانات_خارجية', 'get_external_data' ),
+	'get_ldap_data' => array( 0, 'عرض_بيانات_لداب', 'get_ldap_data' ),
+	'get_db_data' => array( 0, 'عرض_بيانات_قب', 'get_db_data' ),
+	'external_value' => array( 0, 'قيمة_خارجية', 'external_value' ),
+	'for_external_table' => array( 0, 'لجدول_خارجي', 'for_external_table' ),
+	'store_external_table' => array( 0, 'تخزين_جدول_خارجي', 'store_external_table' ),
 );
 
+/** Egyptian Spoken Arabic (مصرى) */
 $magicWords['arz'] = array(
-	'get_external_data'      => array( '0', 'عرض_بيانات_خارجية', 'get_external_data' ),
-	'get_ldap_data'          => array( '0', 'عرض_بيانات_لداب', 'get_ldap_data' ),
-	'get_db_data'            => array( '0', 'عرض_بيانات_قب', 'get_db_data' ),
-	'external_value'         => array( '0', 'قيمة_خارجية', 'external_value' ),
-	'for_external_table'     => array( '0', 'لجدول_خارجي', 'for_external_table' ),
-	'store_external_table'   => array( '0', 'تخزين_جدول_خارجى', 'تخزين_جدول_خارجي', 'store_external_table' ),
+	'get_external_data' => array( 0, 'عرض_بيانات_خارجية', 'get_external_data' ),
+	'get_ldap_data' => array( 0, 'عرض_بيانات_لداب', 'get_ldap_data' ),
+	'get_db_data' => array( 0, 'عرض_بيانات_قب', 'get_db_data' ),
+	'external_value' => array( 0, 'قيمة_خارجية', 'external_value' ),
+	'for_external_table' => array( 0, 'لجدول_خارجي', 'for_external_table' ),
+	'store_external_table' => array( 0, 'تخزين_جدول_خارجى', 'تخزين_جدول_خارجي', 'store_external_table' ),
 );
 
+/** Chechen (Нохчийн) */
 $magicWords['ce'] = array(
-	'get_external_data'      => array( '0', 'схьаэца_архьара_хаамаш', 'получить_внешние_данные', 'get_external_data' ),
-	'get_ldap_data'          => array( '0', 'схьаэца_хаамаш_ldap', 'получить_данные_ldap', 'get_ldap_data' ),
-	'get_db_data'            => array( '0', 'схьаэца_хаамаш_db', 'получить_данные_db', 'get_db_data' ),
-	'external_value'         => array( '0', 'архьара_дозалла', 'внешнее_значение', 'external_value' ),
-	'for_external_table'     => array( '0', 'оцу_архьара_тебалецlаш', 'для_внешней_таблицы', 'for_external_table' ),
+	'get_external_data' => array( 0, 'схьаэца_архьара_хаамаш', 'получить_внешние_данные', 'get_external_data' ),
+	'get_ldap_data' => array( 0, 'схьаэца_хаамаш_ldap', 'получить_данные_ldap', 'get_ldap_data' ),
+	'get_db_data' => array( 0, 'схьаэца_хаамаш_db', 'получить_данные_db', 'get_db_data' ),
+	'external_value' => array( 0, 'архьара_дозалла', 'внешнее_значение', 'external_value' ),
+	'for_external_table' => array( 0, 'оцу_архьара_тебалецlаш', 'для_внешней_таблицы', 'for_external_table' ),
 );
 
+/** Spanish (Español) */
 $magicWords['es'] = array(
-	'get_external_data'      => array( '0', 'obtener_datos_externos', 'get_external_data' ),
-	'get_db_data'            => array( '0', 'obtener_datos_db', 'get_db_data' ),
-	'external_value'         => array( '0', 'valor_externo', 'external_value' ),
+	'get_external_data' => array( 0, 'obtener_datos_externos', 'get_external_data' ),
+	'get_db_data' => array( 0, 'obtener_datos_db', 'get_db_data' ),
+	'external_value' => array( 0, 'valor_externo', 'external_value' ),
 );
 
+/** Japanese (日本語) */
+$magicWords['ja'] = array(
+	'get_external_data' => array( 0, '外部データ取得' ),
+	'get_ldap_data' => array( 0, 'ldapデータ取得' ),
+	'get_db_data' => array( 0, 'dbデータ取得' ),
+	'external_value' => array( 0, '外部値' ),
+	'for_external_table' => array( 0, '外部テーブル用' ),
+	'store_external_table' => array( 0, '外部テーブル格納' ),
+);
+
+/** Malayalam (മലയാളം) */
 $magicWords['ml'] = array(
-	'get_external_data'      => array( '0', 'ബാഹ്യ_വിവരങ്ങൾ_എടുക്കുക', 'get_external_data' ),
-	'external_value'         => array( '0', 'ബാഹ്യ_മൂല്യം', 'external_value' ),
+	'get_external_data' => array( 0, 'ബാഹ്യ_വിവരങ്ങൾ_എടുക്കുക' ),
+	'external_value' => array( 0, 'ബാഹ്യ_മൂല്യം' ),
 );
 
+/** Marathi (मराठी) */
 $magicWords['mr'] = array(
-	'get_external_data'      => array( '0', 'मिळवा_बाह्य_विदा', 'get_external_data' ),
-	'get_ldap_data'          => array( '0', 'मिळवा__एलडॅप_विदा', 'get_ldap_data' ),
-	'for_external_table'     => array( '0', 'बाह्य_सारणी_करिता', 'for_external_table' ),
+	'get_external_data' => array( 0, 'मिळवा_बाह्य_विदा', 'get_external_data' ),
+	'get_ldap_data' => array( 0, 'मिळवा__एलडॅप_विदा', 'get_ldap_data' ),
+	'for_external_table' => array( 0, 'बाह्य_सारणी_करिता', 'for_external_table' ),
 );
 
+/** Nedersaksisch (Nedersaksisch) */
 $magicWords['nds-nl'] = array(
-	'get_external_data'      => array( '0', 'externe_gegevens_ophaolen', 'externe_gegevens_ophalen', 'get_external_data' ),
-	'get_ldap_data'          => array( '0', 'ldap-gegevens_ophaolen', 'ldap_gegevens_ophalen', 'get_ldap_data' ),
-	'get_db_data'            => array( '0', 'db-gegevens_ophaolen', 'db_gegevens_ophalen', 'get_db_data' ),
-	'external_value'         => array( '0', 'externe_weerde', 'externe_waarde', 'external_value' ),
-	'for_external_table'     => array( '0', 'veur_externe_tebel', 'voor_externe_tabel', 'for_external_table' ),
+	'get_external_data' => array( 0, 'externe_gegevens_ophaolen', 'externe_gegevens_ophalen', 'get_external_data' ),
+	'get_ldap_data' => array( 0, 'ldap-gegevens_ophaolen', 'ldap_gegevens_ophalen', 'get_ldap_data' ),
+	'get_db_data' => array( 0, 'db-gegevens_ophaolen', 'db_gegevens_ophalen', 'get_db_data' ),
+	'external_value' => array( 0, 'externe_weerde', 'externe_waarde', 'external_value' ),
+	'for_external_table' => array( 0, 'veur_externe_tebel', 'voor_externe_tabel', 'for_external_table' ),
 );
 
+/** Dutch (Nederlands) */
 $magicWords['nl'] = array(
-	'get_external_data'      => array( '0', 'externe_gegevens_ophalen', 'get_external_data' ),
-	'get_ldap_data'          => array( '0', 'ldap_gegevens_ophalen', 'get_ldap_data' ),
-	'get_db_data'            => array( '0', 'db_gegevens_ophalen', 'get_db_data' ),
-	'external_value'         => array( '0', 'externe_waarde', 'external_value' ),
-	'for_external_table'     => array( '0', 'voor_externe_tabel', 'for_external_table' ),
+	'get_external_data' => array( 0, 'externe_gegevens_ophalen' ),
+	'get_ldap_data' => array( 0, 'ldap_gegevens_ophalen' ),
+	'get_db_data' => array( 0, 'db_gegevens_ophalen' ),
+	'external_value' => array( 0, 'externe_waarde' ),
+	'for_external_table' => array( 0, 'voor_externe_tabel' ),
 );
 
+/** Russian (Русский) */
 $magicWords['ru'] = array(
-	'get_external_data'      => array( '0', 'получить_внешние_данные', 'get_external_data' ),
-	'get_ldap_data'          => array( '0', 'получить_данные_ldap', 'get_ldap_data' ),
-	'get_db_data'            => array( '0', 'получить_данные_db', 'get_db_data' ),
-	'external_value'         => array( '0', 'внешнее_значение', 'external_value' ),
-	'for_external_table'     => array( '0', 'для_внешней_таблицы', 'for_external_table' ),
-	'store_external_table'   => array( '0', 'внешняя_таблица_хранения', 'store_external_table' ),
+	'get_external_data' => array( 0, 'получить_внешние_данные', 'get_external_data' ),
+	'get_ldap_data' => array( 0, 'получить_данные_ldap', 'get_ldap_data' ),
+	'get_db_data' => array( 0, 'получить_данные_db', 'get_db_data' ),
+	'external_value' => array( 0, 'внешнее_значение', 'external_value' ),
+	'for_external_table' => array( 0, 'для_внешней_таблицы', 'for_external_table' ),
+	'store_external_table' => array( 0, 'внешняя_таблица_хранения', 'store_external_table' ),
 );
