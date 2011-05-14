1d71d1575576ce9020118f91ba1a8159c79eeca0
Siebrand Mazeland <siebrand@users.mediawiki.org>
2011-05-14 00:23:17 +0000

    Localisation updates for magic word aliases for extensions from http://translatewiki.net.

--- ExternalData.i18n.magic.php
+++ ExternalData.i18n.magic.php
@@ -1,4 +1,7 @@
 <?php
+/**
+ *
+ */
 
 $magicWords = array();
 
@@ -15,12 +18,13 @@ $magicWords['en'] = array(
 
 /** Arabic (العربية) */
 $magicWords['ar'] = array(
-	'get_external_data' => array( 0, 'عرض_بيانات_خارجية', 'get_external_data' ),
-	'get_ldap_data' => array( 0, 'عرض_بيانات_لداب', 'get_ldap_data' ),
-	'get_db_data' => array( 0, 'عرض_بيانات_قب', 'get_db_data' ),
-	'external_value' => array( 0, 'قيمة_خارجية', 'external_value' ),
-	'for_external_table' => array( 0, 'لجدول_خارجي', 'for_external_table' ),
-	'store_external_table' => array( 0, 'تخزين_جدول_خارجي', 'store_external_table' ),
+	'get_external_data' => array( 0, 'عرض_بيانات_خارجية' ),
+	'get_web_data' => array( 0, 'عرض_بيانات_الويب' ),
+	'get_ldap_data' => array( 0, 'عرض_بيانات_لداب' ),
+	'get_db_data' => array( 0, 'عرض_بيانات_قب' ),
+	'external_value' => array( 0, 'قيمة_خارجية' ),
+	'for_external_table' => array( 0, 'لجدول_خارجي' ),
+	'store_external_table' => array( 0, 'تخزين_جدول_خارجي' ),
 );
 
 /** Egyptian Spoken Arabic (مصرى) */
@@ -59,6 +63,14 @@ $magicWords['ja'] = array(
 	'store_external_table' => array( 0, '外部テーブル格納' ),
 );
 
+/** Macedonian (Македонски) */
+$magicWords['mk'] = array(
+	'get_external_data' => array( 0, 'преземи_надворешни_податоци' ),
+	'external_value' => array( 0, 'надворешна_вредност' ),
+	'for_external_table' => array( 0, 'за_надворешна_табела' ),
+	'store_external_table' => array( 0, 'складирај_надворешна_табела' ),
+);
+
 /** Malayalam (മലയാളം) */
 $magicWords['ml'] = array(
 	'get_external_data' => array( 0, 'ബാഹ്യ_വിവരങ്ങൾ_എടുക്കുക' ),
@@ -84,18 +96,31 @@ $magicWords['nds-nl'] = array(
 /** Dutch (Nederlands) */
 $magicWords['nl'] = array(
 	'get_external_data' => array( 0, 'externe_gegevens_ophalen' ),
+	'get_web_data' => array( 0, 'webgegevens_ophalen' ),
 	'get_ldap_data' => array( 0, 'ldap_gegevens_ophalen' ),
 	'get_db_data' => array( 0, 'db_gegevens_ophalen' ),
 	'external_value' => array( 0, 'externe_waarde' ),
 	'for_external_table' => array( 0, 'voor_externe_tabel' ),
+	'store_external_table' => array( 0, 'opslaan_externe_tabel' ),
 );
 
 /** Russian (Русский) */
 $magicWords['ru'] = array(
-	'get_external_data' => array( 0, 'получить_внешние_данные', 'get_external_data' ),
-	'get_ldap_data' => array( 0, 'получить_данные_ldap', 'get_ldap_data' ),
-	'get_db_data' => array( 0, 'получить_данные_db', 'get_db_data' ),
-	'external_value' => array( 0, 'внешнее_значение', 'external_value' ),
-	'for_external_table' => array( 0, 'для_внешней_таблицы', 'for_external_table' ),
-	'store_external_table' => array( 0, 'внешняя_таблица_хранения', 'store_external_table' ),
+	'get_external_data' => array( 0, 'получить_внешние_данные' ),
+	'get_ldap_data' => array( 0, 'получить_данные_ldap' ),
+	'get_db_data' => array( 0, 'получить_данные_db' ),
+	'external_value' => array( 0, 'внешнее_значение' ),
+	'for_external_table' => array( 0, 'для_внешней_таблицы' ),
+	'store_external_table' => array( 0, 'внешняя_таблица_хранения' ),
+);
+
+/** Serbian Cyrillic ekavian (‪Српски (ћирилица)‬) */
+$magicWords['sr-ec'] = array(
+	'get_external_data' => array( 0, 'добави_спољне_податке' ),
+	'get_web_data' => array( 0, 'добави_веб_податке' ),
+	'get_ldap_data' => array( 0, 'добави_лдап_податке' ),
+	'get_db_data' => array( 0, 'добави_податке_базе' ),
+	'external_value' => array( 0, 'спољна_вредност' ),
+	'for_external_table' => array( 0, 'за_спољну_табелу' ),
+	'store_external_table' => array( 0, 'смести_спољну_табелу' ),
 );
