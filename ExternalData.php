01030dcfb5728960a40188d33f465390d916b89b
yaron <yaron57@gmail.com>
2012-07-19 01:46:40 +0400

    New version: 1.4.1
    Change-Id: Ibd56031f9126172d8eb12d6f48d8b6c2c9ea5705

--- ExternalData.php
+++ ExternalData.php
@@ -12,7 +12,7 @@ if ( !defined( 'MEDIAWIKI' ) ) die();
 $wgExtensionCredits['parserhook'][] = array(
 	'path'           => __FILE__,
 	'name'           => 'External Data',
-	'version'        => '1.4',
+	'version'        => '1.4.1',
 	'author'         => array( 'Yaron Koren', 'Michael Dale', 'David Macdonald' ),
 	'url'            => 'https://www.mediawiki.org/wiki/Extension:External_Data',
 	'descriptionmsg' => 'externaldata-desc',
