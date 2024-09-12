wikimedia/mediawiki-extensions-ExternalData

commit 6a693f5f4248bf4c5d2e7cb172504c56b600fac2

Make External Data compatible with MW 1.39, PHP 8.3 and Docker
Changes:

1) Removed features deprecated in PHP 8.3 and MediaWiki 1.39.
	This does not mean that support for MW 1.35 can
	be dropped now!,
2) Fixed some regressions,
3) Made dockerised applications available via EDConnectorWeb,
	effectively deprecating EDConnectorExe,
4) Created presets of External Data sources:
	4.1) ExternalData\Presets\Base for common pre- and postprocessors,
	4.2) ExternalData\Presets\Test for testing connections
		to some internet resources, databases and local files:
		4.2.1) remote mySQL database 'RFam',
		4.2.2) dockerised PostgreSQL 'dvdrental' test database,
		4.2.3) dockerised Microsoft SQL Server 'Northwind'
			database,
		4.2.4) dockerised MongoDB database with US zip codes,
		4.3.5) this extension's 'extension.json',
		4.3.6) this extension's class hierarchy,
			shown as a diagram (with <graphviz>),
	4.3) ExternalData\Presets\Media for dockerised multimedia
		applications invoked by tags:
		4.3.1) <graphviz>,
		4.3.2) <mscgen>,
		4.3.3) <plantuml>,
		4.3.4) <ploticus>,
		4.3.5) <timeline> (EasyTimeline),
		4.3.6) <gnuplot>,
		4.3.7) <score> (lilypond),
		4.3.8) <barcode> (zint),
		4.3.9) <graph> (Vega, YAML can be used instead of JSON),
		4.3.10) <mermaid>,
		4.3.11) <bpmn>,
		4.3.12) <echarts> (Apache eCharts, YAML can be used
			instead of JSON),
		4.3.13) <mathjax> (implementing limited math support).

		Note that media SVG is generated server-side, yet,
		whenever possible, media client-side behaviour
		can be activated by correct mounting of Docker volumes
		with JavaScript and other assets.

		Most tags support wikilinks. <graphviz> also supports
		images.
	4.4) ExternalData\Presets\Reference for getting reference:
		4.4.1) man pages,
		4.4.2) apk info summaries,
		4.4.3) apk dot graph (with <graphviz>),
		4.4.4) composer show summaries,
		4.4.5) whois reports,
		4.4.6) metadata about YouTube videos, served by
			youtube-dl,
		4.4.7) dockerised mmdb data,

includes/presets/cgi/Dockerfile
@@ -1,0 +1,348 @@
+FROM alpine:latest
+MAINTAINER Alexander Mashin alex_mashin@list.ru
+LABEL description="Universal image for dockerising applications as HTTP CGI servers. To be used with MediaWiki extension External Data"
+LABEL version="0.1"
+
+# Get busybox httpd:
+RUN apk add busybox-extras bash
+ARG APK
+RUN <<-APKADD
+	set -ex && \
+	if [ ! -z "$APK" ]; then
+		apk add --update --no-cache $APK
+	fi
+APKADD
+
+ARG NODE
+ARG NODE_GLOBAL
+RUN <<-NODEJS
+	set -ex && \
+	if [ ! -z "$NODE" ] || [ ! -z "$NODE_GLOBAL" ]; then \
+		apk add --update --no-cache npm && \
+		cd /usr/local/bin && \
+		npm init --yes; \
+	fi && \
+	if [ ! -z "$NODE_GLOBAL" ]; then \
+		npm install -g $NODE_GLOBAL; \
+	fi &&
+	if [ ! -z "$NODE" ]; then \
+		npm install $NODE; \
+	fi &&
+	if [ -d /usr/local/bin/node_modules/puppeteer ]; then \
+		echo '{ "headless": 1, "timeout": 30000, "executablePath": "/usr/bin/chromium", "args": [ "--no-sandbox", "--disable-gpu" ] }' > /usr/local/bin/.puppeteerrc.json; \
+	fi
+NODEJS
+
+ARG PIP
+RUN <<-PIPINSTALL
+	set -ex && \
+	if [ ! -z "$PIP" ]; then \
+		apk add --update --no-cache py3-pip && \
+		python -m  pip install --break-system-packages $PIP; \
+	fi
+PIPINSTALL
+
+ARG GO
+ENV GOPATH=/usr/local/go
+RUN <<-GOINSTALL
+	set -ex && \
+	if [ ! -z "$GO" ]; then \
+		apk add --update --no-cache go && \
+		go install $GO && \
+		go clean && apk del go; \
+	fi
+GOINSTALL
+
+# Downloads all URLS; then replaces all archives in current directory with their contents:
+COPY --chmod=777 <<-'GET_ALL' /usr/local/bin/get_all.sh
+#!/bin/sh
+	set -eux
+	URLS="$1"
+	for URL in $URLS; do \
+		wget "$URL"; \
+	done && \
+	for FILE in *\?*; do \
+		if [ -f "$FILE" ]; then \
+			mv "$FILE" "${FILE%\?*}"; \
+		fi; \
+	done && \
+	for TAR_GZ in *.tar.gz; do \
+		if [ -f "$TAR_GZ" ]; then \
+			tar -xzf "$TAR_GZ" && rm "$TAR_GZ"; \
+		fi; \
+	done && \
+	for ZIP in *.zip; do \
+		if [ -f "$ZIP" ]; then \
+			unzip "$ZIP" && rm "$ZIP"; \
+		fi; \
+	done
+GET_ALL
+
+ARG JAR
+RUN <<-JAR
+	set -ex && \
+	if [ ! -z "$JAR" ]; then \
+		apk add --update --no-cache openjdk11 && mkdir -p /usr/share/java && \
+		cd /usr/share/java && \
+		/usr/local/bin/get_all.sh "$JAR"; \
+	fi
+JAR
+
+ARG BINARY
+RUN <<-BINARY
+	set -ex && \
+	if [ ! -z "$BINARY" ]; then \
+		cd /usr/local/bin && \
+		/usr/local/bin/get_all.sh "$BINARY" && \
+		for FILE in *; do \
+			chmod a+rx "$FILE"; \
+		done; \
+	fi
+BINARY
+
+ARG WGET
+RUN <<-WGET
+	set -ex && \
+	if [ ! -z "$WGET" ]; then \
+		mkdir -p /usr/share/downloads && cd /usr/share/downloads && \
+		/usr/local/bin/get_all.sh "$WGET"; \
+	fi
+WGET
+
+ARG SRC
+ARG GIT
+ARG SRC_LANG=C
+RUN <<-SRC
+	set -ex && \
+	if [ ! -z "$SRC" ] || [ ! -z "$GIT" ]; then \
+		if [ $SRC_LANG == 'go' ]; then \
+			BUILD='go make git'; \
+		elif [ $SRC_LANG == 'C' ]; then \
+			BUILD='flex bison gcc musl-dev make'; \
+		fi && \
+		if [ ! -z "$GIT" ]; then \
+			BUILD="$BUILD git"; \
+		fi && \
+		apk add --update --no-cache $BUILD && \
+		mkdir -p /usr/local/bin && mkdir -p /src && cd /src && \
+		if [ ! -z "$GIT" ]; then \
+			for URL in $GIT; do \
+				git clone --single-branch --branch=master "$URL"; \
+			done; \
+		else \
+			if [ ! -z "$SRC" ]; then \
+				/usr/local/bin/get_all.sh "$SRC"; \
+			fi; \
+		fi && \
+		for DIR in ./*/; do \
+			DIR=${DIR%/*} && cd "$DIR" && \
+			if [ ! -f './configure' ] && [ ! -f './Makefile' ] && [ ! -f './Makefile.*' ]; then \
+				cd src; \
+			fi && \
+			if [ -f './configure' ]; then \
+				./configure; \
+			fi && \
+			make && ( \
+				make install || \
+				for FILE in ./*; do \
+					if [ -f "$FILE" ] && [ -x "$FILE" ]; then \
+						cp "$FILE" "/usr/bin/";
+					fi; \
+				done \
+			) && ls -la . && make clean; \
+		done && \
+		apk del $BUILD && cd / && rm -r /src
+	fi
+SRC
+
+ARG STARTUP
+RUN set -ex && ( : ; $STARTUP )
+
+ARG SCRIPT
+RUN <<-SCRIPT
+	set -ex && \
+	if [ ! -z "$SCRIPT" ]; then \
+		echo "$SCRIPT" > /usr/local/bin/script && chmod +x /usr/local/bin/script; \
+	fi
+SCRIPT
+
+COPY --chmod=777 <<-FUNC /usr/local/bin/cgi_functions.sh
+	#!/bin/bash
+	function urldecode() {
+		httpd -d "\$@"
+	}
+
+	# Parse query strings, set variables:
+	function parse_query() {
+		QUERY_STRING="\$1"
+
+		SAVE_IFS=\$IFS
+		IFS='&'
+		ASSIGNMENTS=(\$QUERY_STRING)
+		for (( i=0; i<\${#ASSIGNMENTS[@]}; i+=1 )); do
+			IFS='='
+			PAIR=(\${ASSIGNMENTS[i]})
+			KEY=\${PAIR[0]}
+			VALUE=\${PAIR[1]}
+			for (( j=2; j<\${#PAIR[@]}; j+=1 )); do
+				VALUE="\$VALUE=\${PAIR[j]}"
+			done
+			declare -g "\$KEY"="\$( urldecode "\$VALUE" )"
+		done
+		IFS=\$SAVE_IFS
+	}
+
+	function debug_info() {
+		COMMAND="\$1"
+		FILTER_COMMAND="\$2"
+		QUERY_STRING="\$3"
+		REQUEST_METHOD="\$4"
+		STDOUT="\$5"
+		STDERR="\$6"
+
+		echo "Query was \$QUERY_STRING"; echo ''
+		echo 'Command is \$COMMAND'; echo ''
+		echo 'Filter command is \$FILTER_COMMAND'; echo ''
+		if [ "\$REQUEST_METHOD" == 'POST' ]; then
+			echo 'stdin: '; cat; echo '';
+		fi
+		echo 'stdout:'; cat "\$STDOUT"; echo ''
+		echo 'stderr:'; cat "\$STDERR"; echo ''
+	}
+
+	# If parameters do not pass the filter or debug mode is on:
+	function filter_result() {
+		STDOUT="\$1"
+		STDERR="\$2"
+		DEBUG="\$3"
+		COMMAND="\$4"
+		FILTER_COMMAND="\$5"
+		QUERY_STRING="\$6"
+		REQUEST_METHOD="\$7"
+
+		if [ -s "\$STDERR" ]; then
+			echo 'Status: 502 Bad Gateway'; echo '';
+			echo 'Wrong query arguments'; echo ''
+			if [ ! -z "\$DEBUG" ]; then
+				debug_info "$COMMAND" "$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD" "\$STDOUT" "\$STDERR"
+			fi
+		else
+			if [ ! -z "\$DEBUG" ]; then
+				debug_info"$COMMAND" "$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD" "\$STDOUT" "\$STDERR"
+			fi
+		fi
+		rm -f "\$STDOUT" "\$STDERR"
+	}
+
+	function command_result() {
+		CONTENT_TYPE="\$1"
+		STDOUT="\$2"
+		STDERR="\$3"
+		DEBUG="\$4"
+		ERRORS="\$5"
+		COMMAND="\$6"
+		FILTER_COMMAND="\$7"
+		QUERY_STRING="\$8"
+		REQUEST_METHOD="\$9"
+
+		if [ -z "\$DEBUG" ]; then
+			if [ -s "\$STDOUT" ] && ( [ ! -s "\$STDERR" ] || [ "\$ERRORS" == 'FATAL' ] ) || [ "\$ERRORS" == 'IGNORE' ]; then
+				echo 'Content-type: \$CONTENT_TYPE; charset=UTF-8'
+				echo 'Status: 200 OK';
+				echo '';
+				cat "\$STDOUT"
+			else
+				echo 'Status: 502 Bad Gateway';
+				echo '';
+				cat "\$STDERR"
+			fi
+		else
+			debug_info "$COMMAND" "$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD" "\$STDOUT" "\$STDERR"
+		fi
+		cat "\$STDERR" > /dev/stderr
+		rm -f "\$STDOUT" "\$STDERR"
+	}
+FUNC
+
+ARG COMMAND="echo 'Environment variables:'; env; echo 'Standard input:'; cat"
+ARG FILTER_COMMAND=''
+ARG CONTENT_TYPE='text/plain'
+ARG CGI=cgi.sh
+ENV ERRORS=ALL
+ENV DEBUG=''
+COPY --chmod=777 <<-CGI /www/cgi-bin/$CGI
+	#!/bin/bash
+	: '
+	Do not set -eux.
+	$COMMAND will receive stdin from /www/cgi-bin/\$SCRIPT
+	https://oldforum.puppylinux.com/viewtopic.php?t=115252
+	'
+
+ 	source /usr/local/bin/cgi_functions.sh
+
+	# Parse query strings, set variables:
+	parse_query "\${QUERY_STRING:-}"
+
+	# This is to reliably escape any quotes:
+	COMMAND=$( cat <<-'SCMD'
+		$COMMAND
+	SCMD
+	)
+	FILTER_COMMAND=$( cat <<-'FCMD'
+		$FILTER_COMMAND
+	FCMD
+	)
+
+	# Apply FILTER_COMMAND to check variables:
+	if [ ! -z '$FILTER_COMMAND' ]; then
+		STDOUT=\$(mktemp -u)
+		STDERR=\$(mktemp -u)
+		( :; $FILTER_COMMAND ) 1>"\$STDOUT" 2>"\$STDERR"
+		filter_result "\$STDOUT" "\$STDERR" "\$DEBUG" \\
+			"\$COMMAND" "\$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD"
+	fi
+
+	if [ "\${REQUEST_METHOD:-}" == 'GET' ]; then
+		# Close stdin:
+		exec 0<&-
+	fi
+
+	STDOUT=\$(mktemp -u)
+	STDERR=\$(mktemp -u)
+
+	# Actually run the command:
+	( $COMMAND ) 1>"\$STDOUT" 2>"\$STDERR"
+
+	command_result '$CONTENT_TYPE' "\$STDOUT" "\$STDERR" "\$DEBUG" "\$ERRORS" \\
+		"\$COMMAND" "\$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD"
+
+CGI
+
+ARG COMMAND="echo 'Environment variables:'; env; echo 'Standard input:'; cat"
+ARG VERSION_COMMAND
+COPY --chmod=777 <<-VERSION /www/cgi-bin/version.sh
+	#!/bin/sh
+	set -eu
+	echo 'Content-type: text/plain; charset=UTF-8'
+	echo 'Status: 200 OK'
+	echo ''
+	if [ ! -z '$VERSION_COMMAND' ]; then
+		VC='$VERSION_COMMAND'
+	else
+		COMMAND='$COMMAND'
+		VC="\${COMMAND%% *} --version || \${COMMAND%% *} -v || \${COMMAND%% *} -V"
+	fi
+	eval "\$VC" 2>&1 # Some programs, e.g., graphviz, print version info to stderr out of principle.
+VERSION
+
+COPY --chmod=777 <<-'START' /usr/local/bin/start.sh
+	#!/bin/sh
+	set -eux
+	exec httpd -v -p 80 -h /www -f
+START
+
+EXPOSE 80
+
+RUN adduser -D www-data -G www-data
+USER www-data
+ENTRYPOINT ["/usr/local/bin/start.sh"]

includes/presets/mssqlserver/Dockerfile
@@ -1,0 +1,70 @@
+ARG VERSION=2017-latest
+FROM mcr.microsoft.com/mssql/server:$VERSION
+
+MAINTAINER Alexander Mashin alex_mashin@list.ru
+LABEL description="Microsoft SQL Server image with preinstalled Northwind database for testing pirposes"
+LABEL version="0.2"
+
+########################
+# Developer tools      #
+########################
+
+# install zsh and oh-my-zsh
+USER root
+RUN apt update && apt install -y zsh wget git \
+  && sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh-O -)" \
+  && git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions \
+  && echo "source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh" >> ~/.zshrc \
+  && echo "ZSH_THEME=af-magic" >> ~/.zshrc
+
+# install dev tools
+RUN apt install -y vim tmux
+
+########################
+# Configure SQL Server #
+########################
+
+ENV ACCEPT_EULA="Y"
+ENV MSSQL_PID="Express"
+
+ENV PATH=$PATH:/opt/mssql-tools/bin
+
+WORKDIR "/root"
+
+########################
+# Setup Northwind Data #
+########################
+
+ARG TEST_DB_SQL_URL="https://raw.githubusercontent.com/Microsoft/sql-server-samples/master/samples/databases/northwind-pubs/instnwnd.sql"
+RUN wget "$TEST_DB_SQL_URL" -O "/usr/share/$( basename $TEST_DB_SQL_URL )"
+ARG TEST_DB_SQL_URL="https://raw.githubusercontent.com/Microsoft/sql-server-samples/master/samples/databases/northwind-pubs/instnwnd.sql"
+ENV ACCEPT_EULA=Y \
+	MSSQL_SA_PASSWORD_FILE=/run/secrets/sa_password \
+	TEST_DB=Northwind
+COPY --chmod=777 <<-ENTRY /bin/entrypoint.sh
+	#!bin/sh
+	set -eux
+	( \
+		sleep 90s && \
+		if [ -f "\$MSSQL_SA_PASSWORD_FILE" ]; then \
+			SA_PASSWD="\$(cat "\$MSSQL_SA_PASSWORD_FILE")"; \
+		else \
+			SA_PASSWD="\$MSSQL_SA_PASSWORD"; \
+		fi && \
+		/opt/mssql-tools18/bin/sqlcmd -C -S 'localhost' -U 'sa' -P "\$SA_PASSWD" -Q "CREATE DATABASE \$TEST_DB;" && \
+		/opt/mssql-tools18/bin/sqlcmd -C -S 'localhost' -U 'sa' -P "\$SA_PASSWD" -d "\$TEST_DB" -i "/usr/share/\$( basename '$TEST_DB_SQL_URL' )" && \
+		TEST_USER="\$(cat /run/secrets/user)" && \
+		TEST_PASSWD="\$(cat /run/secrets/password)" && \
+		/opt/mssql-tools18/bin/sqlcmd -C -S 'localhost' -U 'sa' -P "\$SA_PASSWD" -Q "\
+			CREATE LOGIN \$TEST_USER WITH PASSWORD = '\$TEST_PASSWD'; \
+			USE \$TEST_DB; \
+			CREATE USER \$TEST_USER FOR LOGIN \$TEST_USER; \
+			EXEC sp_addrolemember 'db_datareader', '\$TEST_USER'; \
+		";
+	) & \
+	/opt/mssql/bin/sqlservr
+ENTRY
+
+CMD /bin/sh /bin/entrypoint.sh
+
+EXPOSE 1433

includes/presets/postgresql/Dockerfile
@@ -1,0 +1,36 @@
+ARG VERSION=16.4
+FROM postgres:$VERSION-alpine
+
+MAINTAINER Alexander Mashin alex_mashin@list.ru
+LABEL description="PostrgreSQL image with preinstalled Northwind database for testing pirposes"
+LABEL version="0.2"
+
+ARG TEST_DB_SQL_URL=https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
+RUN ZIP="$(basename "$TEST_DB_SQL_URL")" && \
+	wget "$TEST_DB_SQL_URL" -O "/usr/share/$ZIP" && \
+	unzip "/usr/share/$ZIP" -d /usr/share && \
+	rm "/usr/share/$ZIP"
+
+ARG TEST_DB_SQL_URL=https://www.postgresqltutorial.com/wp-content/uploads/2019/05/dvdrental.zip
+ENV POSTGRES_PASSWORD_FILE=/run/secrets/postgres_password
+
+COPY --chmod=777 <<-ENTRY /docker-entrypoint-initdb.d/install_test.sh
+	#!bin/sh
+	set -eux
+	
+	# Restore the dvdrental database:
+	URL='$TEST_DB_SQL_URL'
+	TEST_DB="\$(basename \${URL%%.zip})"
+	psql -U postgres -c "CREATE DATABASE \$TEST_DB;"
+	pg_restore -U postgres -d "\$TEST_DB" "/usr/share/\$TEST_DB.tar"
+	
+	# Give the test user reading rights on dvdrental:
+	TEST_USER="\$(cat /run/secrets/user)"
+	TEST_PASSWD="\$(cat /run/secrets/password)"
+
+	psql -U postgres -d "\$TEST_DB" -c " \
+		CREATE ROLE \$TEST_USER WITH LOGIN PASSWORD '\$TEST_PASSWD'; \
+		GRANT CONNECT ON DATABASE \$TEST_DB TO \$TEST_USER; \
+		GRANT SELECT ON ALL TABLES IN SCHEMA public TO \$TEST_USER; \
+	"
+ENTRY

Gruntfile.js
@@ -14,7 +14,8 @@ module.exports = function ( grunt ) {
 			all: [
 				'**/*.{js,json}',
 				'!node_modules/**',
-				'!vendor/**'
+				'!vendor/**',
+				'!includes/presets'
 			]
 		}
 	} );

includes/EDJsonObject.php
@@ -97,7 +97,7 @@ class EDJsonObject {
 	 *
 	 * @return void
 	 *
-	 * @throws MWException
+	 * @throws EDParserException
 	 */
 	public function __construct( $json = null ) {
 		if ( $json === null ) {
@@ -105,14 +105,14 @@ class EDJsonObject {
 		} elseif ( is_string( $json ) ) {
 			$this->jsonObject = json_decode( $json, true );
 			if ( $this->jsonObject === null ) {
-				throw new MWException( wfMessage( 'externaldata-invalid-format', 'JSON' )->text() );
+				throw new EDParserException( 'externaldata-invalid-format', 'JSON', $json );
 			}
 		} elseif ( is_array( $json ) ) {
 			$this->jsonObject = $json;
 		} elseif ( is_object( $json ) ) {
 			$this->jsonObject = json_decode( json_encode( $json ), true );
 		} else {
-			throw new MWException( wfMessage( 'externaldata-invalid-format', 'JSON' )->text() );
+			throw new EDParserException( wfMessage( 'externaldata-invalid-format', 'JSON' )->text() );
 		}
 	}
 
@@ -138,8 +138,7 @@ class EDJsonObject {
 	 */
 	public function get( $jsonPath ) {
 		$this->hasDiverged = false;
-		$result = $this->getReal( $this->jsonObject, $jsonPath );
-		return $result;
+		return $this->getReal( $this->jsonObject, $jsonPath );
 	}
 
 	/**

includes/EDParserFunctions.php
@@ -72,17 +72,20 @@ class EDParserFunctions {
 	/**
 	 * Actually get the external data.
 	 *
-	 * @param ?Title $title Page title.
+	 * @param Title $title Page title.
 	 * @param string|null $name Parser function name.
 	 * @param array $args Parser function parameters ($parser not included).
 	 *
 	 * @return string|array|null Return an array of values on success, an error message otherwise.
 	 */
-	private static function get( $title, $name, array $args ) {
+	private static function get( Title $title, ?string $name, array $args ) {
 		// Unset self::$values if the current page changed during this script run.
 		// Looks like it is relevant for maintenance scripts.
 		if ( $title ) {
 			self::clearValuesIfNecessary( $title->getText() );
+		} else {
+			// Hopefully, this code is never reached.
+			$title = Title::newMainPage();
 		}
 
 		$connector = EDConnectorBase::getConnector( $name, self::parseParams( $args ), $title );
@@ -105,13 +108,13 @@ class EDParserFunctions {
 	 * Also includes all the boilerplate code that processes parameters,
 	 * saves external values, etc.
 	 *
-	 * @param ?Title $title Parser object.
+	 * @param Title $title Parser object.
 	 * @param string|null $name Parser function name.
 	 * @param array $args Parser function parameters ($parser not included).
 	 *
 	 * @return string|null Return null on success, an error message otherwise.
 	 */
-	public static function fetch( $title, $name, array $args ) {
+	public static function fetch( Title $title, ?string $name, array $args ) {
 		$result = self::get( $title, $name, $args );
 		if ( is_array( $result ) ) {
 			// An array of values, not an error message.
@@ -187,16 +190,11 @@ class EDParserFunctions {
 	/**
 	 * Emulate {{#get_external_data:}} call.
 	 * @param array &$args
-	 * @param ?Title $title
+	 * @param Title $title
 	 * @return null|string
 	 */
 	private static function emulateGetExternalData( array &$args, $title ) {
-		if (
-			isset( $args['source'] ) ||
-			isset( $args['url'] ) ||
-			isset( $args['text'] ) ||
-			isset( $args['program'] )
-		) {
+		if ( EDConnectorBase::sourceSet( $args ) ) {
 			// If {{#for_external_table:}} is called in standalone mode, there is no shared context,
 			// therefore, emulate {{#clear_external_data:}}.
 			self::actuallyClearExternalData( [] );
@@ -226,7 +224,8 @@ class EDParserFunctions {
 			array_shift( $args );
 		}
 		$args['data'] ??= "$variable=$variable";
-		$fetched = self::emulateGetExternalData( $args, $parser->getTitle() );
+		$title = method_exists( 'Parser', 'getPage' ) ? $parser->getPage() : $parser->getTitle();
+		$fetched = self::emulateGetExternalData( $args, $title );
 		if ( $fetched ) {
 			// There is an error.
 			return $fetched;
@@ -382,7 +381,8 @@ class EDParserFunctions {
 				$data_params['data'] = $variables;
 			}
 
-			$fetched = self::emulateGetExternalData( $data_params, $parser->getTitle() );
+			$title = method_exists( 'Parser', 'getPage' ) ? $parser->getPage() : $parser->getTitle();
+			$fetched = self::emulateGetExternalData( $data_params, $title );
 			if ( $fetched ) {
 				// There is an error.
 				return $fetched;
@@ -420,7 +420,7 @@ class EDParserFunctions {
 	 * Actually display external table.
 	 *
 	 * @param array $args
-	 * @param ?Title $title
+	 * @param Title $title
 	 * @return array
 	 */
 	private static function actuallyDisplayExternalTable( array $args, $title ): array {
@@ -490,7 +490,8 @@ class EDParserFunctions {
 		$params = func_get_args();
 		array_shift( $params ); // we already know the $parser ...
 		$args = self::parseParams( $params ); // parse params into name-value pairs
-		$result = self::actuallyDisplayExternalTable( $args, $parser->getTitle() );
+		$title = method_exists( 'Parser', 'getPage' ) ? $parser->getPage() : $parser->getTitle();
+		$result = self::actuallyDisplayExternalTable( $args, $title );
 		if ( isset( $result['error'] ) ) {
 			// Message is created here rather than in EDParserFunctions::actuallyDisplayExternalTable()
 			//      to clear that method from MediaWiki installation-dependent code and make it testable.
@@ -514,7 +515,8 @@ class EDParserFunctions {
 		array_shift( $params ); // we already know the $parser ...
 		$args = self::parseParams( $params ); // parse params into name-value pairs
 
-		$fetched = self::emulateGetExternalData( $args, $parser->getTitle() );
+		$title = method_exists( 'Parser', 'getPage' ) ? $parser->getPage() : $parser->getTitle();
+		$fetched = self::emulateGetExternalData( $args, $title );
 		if ( $fetched ) {
 			// There is an error.
 			return [ 'error' => $fetched ];

commit 4e4f34126a95136101721516053fe691cc8c0319

Fix regressions introduced in 6a693f5f4248
Regressions were introduced during undertested refactoring of:
1) Media postprocessor extracting HTML from <html><body>…</body></html>,
2) Fancy bash functions in cgi/Dockerfile (mainly, error handling).

Change-Id: Id8ced01a917e9c85628cc7db4d061f10c715294a

includes/presets/Media.php
@@ -4,7 +4,6 @@ namespace ExternalData\Presets;
 
 use CoreParserFunctions;
 use DOMDocument;
-use DOMXPath;
 use MediaWiki\Languages\Data\Names;
 use MediaWiki\MediaWikiServices;
 use MWException;
@@ -514,15 +513,13 @@ class Media extends Base {
 	 * @return string The stripped SVG.
 	 */
 	public static function innerHtml( string $html ): string {
-		$doc = new DOMDocument( '1.0', 'UTF-8' );
-		$doc->loadHTML( $html, LIBXML_NOERROR );
-		$domxpath = new DOMXPath( $doc );
-		$entry = $domxpath->evaluate( '/html/body' )->item( 0 );
-		$nodes_array = [];
-		foreach ( $entry->childNodes as $node ) {
-			$nodes_array[] = $entry->ownerDocument->saveHTML( $node );
+		// Resorting to the heinous art of parsing XML with regular expressions
+		// as DOMDocument::loadHTML breaks HTML5 in some cases.
+		// In particular, it dislikes minuses and primes in MathML.
+		if ( preg_match( '%<body[^>]*>(.+)</body>%s', $html, $matches ) ) {
+			return $matches[1];
 		}
-		return implode( '', $nodes_array );
+		return $html;
 	}
 
 	/**
@@ -678,7 +675,7 @@ class Media extends Base {
 		if ( $params['nomenu'] === false && !$math_jax_included ) {
 			$math_jax_included = true;
 			$script = "\n" . '<script type="text/javascript" async src="'
-					. "{$params['scripts']}/tex-mml-chtml.js" . '" />';
+					. "{$params['scripts']}/tex-mml-chtml.js" . '"></script>';
 		} else {
 			$script = '';
 		}

includes/presets/cgi/Dockerfile
@@ -4,12 +4,12 @@ LABEL description="Universal image for dockerising applications as HTTP CGI serv
 LABEL version="0.1"
 
 # Get busybox httpd:
-RUN apk add busybox-extras bash
+RUN apk add --update --no-cache busybox-extras bash
 ARG APK
 RUN <<-APKADD
 	set -ex && \
 	if [ ! -z "$APK" ]; then
-		apk add --update --no-cache $APK
+		apk add --no-cache $APK
 	fi
 APKADD
 
@@ -18,7 +18,7 @@ ARG NODE_GLOBAL
 RUN <<-NODEJS
 	set -ex && \
 	if [ ! -z "$NODE" ] || [ ! -z "$NODE_GLOBAL" ]; then \
-		apk add --update --no-cache npm && \
+		apk add --no-cache npm && \
 		cd /usr/local/bin && \
 		npm init --yes; \
 	fi && \
@@ -37,7 +37,7 @@ ARG PIP
 RUN <<-PIPINSTALL
 	set -ex && \
 	if [ ! -z "$PIP" ]; then \
-		apk add --update --no-cache py3-pip && \
+		apk add --no-cache py3-pip && \
 		python -m  pip install --break-system-packages $PIP; \
 	fi
 PIPINSTALL
@@ -47,7 +47,7 @@ ENV GOPATH=/usr/local/go
 RUN <<-GOINSTALL
 	set -ex && \
 	if [ ! -z "$GO" ]; then \
-		apk add --update --no-cache go && \
+		apk add --no-cache go && \
 		go install $GO && \
 		go clean && apk del go; \
 	fi
@@ -82,7 +82,7 @@ ARG JAR
 RUN <<-JAR
 	set -ex && \
 	if [ ! -z "$JAR" ]; then \
-		apk add --update --no-cache openjdk11 && mkdir -p /usr/share/java && \
+		apk add --no-cache openjdk11 && mkdir -p /usr/share/java && \
 		cd /usr/share/java && \
 		/usr/local/bin/get_all.sh "$JAR"; \
 	fi
@@ -123,7 +123,7 @@ RUN <<-SRC
 		if [ ! -z "$GIT" ]; then \
 			BUILD="$BUILD git"; \
 		fi && \
-		apk add --update --no-cache $BUILD && \
+		apk add --no-cache $BUILD && \
 		mkdir -p /usr/local/bin && mkdir -p /src && cd /src && \
 		if [ ! -z "$GIT" ]; then \
 			for URL in $GIT; do \
@@ -166,101 +166,106 @@ RUN <<-SCRIPT
 	fi
 SCRIPT
 
-COPY --chmod=777 <<-FUNC /usr/local/bin/cgi_functions.sh
+COPY --chmod=777 <<-'FUNC' /usr/local/bin/cgi_functions.sh
 	#!/bin/bash
 	function urldecode() {
-		httpd -d "\$@"
+		httpd -d "$@"
 	}
 
 	# Parse query strings, set variables:
 	function parse_query() {
-		QUERY_STRING="\$1"
+		QUERY_STRING="$1"
 
-		SAVE_IFS=\$IFS
+		SAVE_IFS=$IFS
 		IFS='&'
-		ASSIGNMENTS=(\$QUERY_STRING)
-		for (( i=0; i<\${#ASSIGNMENTS[@]}; i+=1 )); do
+		ASSIGNMENTS=($QUERY_STRING)
+		for (( i=0; i<${#ASSIGNMENTS[@]}; i+=1 )); do
 			IFS='='
-			PAIR=(\${ASSIGNMENTS[i]})
-			KEY=\${PAIR[0]}
-			VALUE=\${PAIR[1]}
-			for (( j=2; j<\${#PAIR[@]}; j+=1 )); do
-				VALUE="\$VALUE=\${PAIR[j]}"
+			PAIR=(${ASSIGNMENTS[i]})
+			KEY=${PAIR[0]}
+			VALUE=${PAIR[1]}
+			for (( j=2; j<${#PAIR[@]}; j+=1 )); do
+				VALUE="$VALUE=${PAIR[j]}"
 			done
-			declare -g "\$KEY"="\$( urldecode "\$VALUE" )"
+			declare -g "$KEY"="$( urldecode "$VALUE" )"
 		done
 		IFS=\$SAVE_IFS
 	}
 
 	function debug_info() {
-		COMMAND="\$1"
-		FILTER_COMMAND="\$2"
-		QUERY_STRING="\$3"
-		REQUEST_METHOD="\$4"
-		STDOUT="\$5"
-		STDERR="\$6"
+		COMMAND="$1"
+		FILTER_COMMAND="$2"
+		QUERY_STRING="$3"
+		REQUEST_METHOD="$4"
+		STDOUT="$5"
+		STDERR="$6"
+		STDIN="$7"
 
-		echo "Query was \$QUERY_STRING"; echo ''
-		echo 'Command is \$COMMAND'; echo ''
-		echo 'Filter command is \$FILTER_COMMAND'; echo ''
-		if [ "\$REQUEST_METHOD" == 'POST' ]; then
-			echo 'stdin: '; cat; echo '';
+		echo "Query was $QUERY_STRING"; echo ''
+		echo "Command is $COMMAND"; echo ''
+		echo "Filter command is $FILTER_COMMAND"; echo ''
+		if [ ! -z "$STDIN" ]; then
+			echo "stdin: $STDIN"
 		fi
-		echo 'stdout:'; cat "\$STDOUT"; echo ''
-		echo 'stderr:'; cat "\$STDERR"; echo ''
+		echo 'stdout:'; cat "$STDOUT"; echo ''
+		echo 'stderr:'; cat "$STDERR"; echo ''
 	}
 
 	# If parameters do not pass the filter or debug mode is on:
 	function filter_result() {
-		STDOUT="\$1"
-		STDERR="\$2"
-		DEBUG="\$3"
-		COMMAND="\$4"
-		FILTER_COMMAND="\$5"
-		QUERY_STRING="\$6"
-		REQUEST_METHOD="\$7"
+		STDOUT="$1"
+		STDERR="$2"
+		DEBUG="$3"
+		COMMAND="$4"
+		FILTER_COMMAND="$5"
+		QUERY_STRING="$6"
+		REQUEST_METHOD="$7"
 
-		if [ -s "\$STDERR" ]; then
+		if [ -s "$STDERR" ]; then
 			echo 'Status: 502 Bad Gateway'; echo '';
 			echo 'Wrong query arguments'; echo ''
-			if [ ! -z "\$DEBUG" ]; then
-				debug_info "$COMMAND" "$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD" "\$STDOUT" "\$STDERR"
+			if [ ! -z "$DEBUG" ]; then
+				debug_info "$COMMAND" "$FILTER_COMMAND" "$QUERY_STRING" "$REQUEST_METHOD" "$STDOUT" "$STDERR"
 			fi
 		else
-			if [ ! -z "\$DEBUG" ]; then
-				debug_info"$COMMAND" "$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD" "\$STDOUT" "\$STDERR"
+			if [ ! -z "$DEBUG" ]; then
+				echo 'Status: 502 Bad Gateway'; echo '';
+				debug_info "$COMMAND" "$FILTER_COMMAND" "$QUERY_STRING" "$REQUEST_METHOD" "$STDOUT" "$STDERR"
 			fi
 		fi
-		rm -f "\$STDOUT" "\$STDERR"
+		rm -f "$STDOUT" "$STDERR"
 	}
 
 	function command_result() {
-		CONTENT_TYPE="\$1"
-		STDOUT="\$2"
-		STDERR="\$3"
-		DEBUG="\$4"
-		ERRORS="\$5"
-		COMMAND="\$6"
-		FILTER_COMMAND="\$7"
-		QUERY_STRING="\$8"
-		REQUEST_METHOD="\$9"
+		CONTENT_TYPE="$1"
+		STDOUT="$2"
+		STDERR="$3"
+		DEBUG="$4"
+		ERRORS="$5"
+		COMMAND="$6"
+		FILTER_COMMAND="$7"
+		QUERY_STRING="$8"
+		REQUEST_METHOD="$9"
+		STDIN="${10}"
 
-		if [ -z "\$DEBUG" ]; then
-			if [ -s "\$STDOUT" ] && ( [ ! -s "\$STDERR" ] || [ "\$ERRORS" == 'FATAL' ] ) || [ "\$ERRORS" == 'IGNORE' ]; then
-				echo 'Content-type: \$CONTENT_TYPE; charset=UTF-8'
+		if [ -z "$DEBUG" ]; then
+			if [ -s "$STDOUT" ] && ( [ ! -s "$STDERR" ] || [ "$ERRORS" == 'FATAL' ] ) || [ "$ERRORS" == 'IGNORE' ]; then
+				echo "Content-type: $CONTENT_TYPE; charset=UTF-8"
 				echo 'Status: 200 OK';
 				echo '';
-				cat "\$STDOUT"
+				cat "$STDOUT"
 			else
 				echo 'Status: 502 Bad Gateway';
 				echo '';
-				cat "\$STDERR"
+				cat "$STDERR"
 			fi
 		else
-			debug_info "$COMMAND" "$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD" "\$STDOUT" "\$STDERR"
+			echo 'Status: 502 Bad Gateway'; echo '';
+			echo 'Debug mode'
+			debug_info "$COMMAND" "$FILTER_COMMAND" "$QUERY_STRING" "$REQUEST_METHOD" "$STDOUT" "$STDERR" "$STDIN"
 		fi
-		cat "\$STDERR" > /dev/stderr
-		rm -f "\$STDOUT" "\$STDERR"
+		cat "$STDERR" > /dev/stderr
+		rm -f "$STDOUT" "$STDERR"
 	}
 FUNC
 
@@ -293,28 +298,31 @@ COPY --chmod=777 <<-CGI /www/cgi-bin/$CGI
 	FCMD
 	)
 
+	if [ "\${REQUEST_METHOD:-}" == 'GET' ]; then
+		# Close stdin:
+		exec 0<&-
+		STDIN=''
+	else
+		STDIN=\$( cat )
+	fi
+
 	# Apply FILTER_COMMAND to check variables:
 	if [ ! -z '$FILTER_COMMAND' ]; then
 		STDOUT=\$(mktemp -u)
 		STDERR=\$(mktemp -u)
-		( :; $FILTER_COMMAND ) 1>"\$STDOUT" 2>"\$STDERR"
+		( :; $FILTER_COMMAND ) <<< "$STDIN" 1>"\$STDOUT" 2>"\$STDERR"
 		filter_result "\$STDOUT" "\$STDERR" "\$DEBUG" \\
 			"\$COMMAND" "\$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD"
-	fi
-
-	if [ "\${REQUEST_METHOD:-}" == 'GET' ]; then
-		# Close stdin:
-		exec 0<&-
 	fi
 
 	STDOUT=\$(mktemp -u)
 	STDERR=\$(mktemp -u)
 
 	# Actually run the command:
-	( $COMMAND ) 1>"\$STDOUT" 2>"\$STDERR"
+	( $COMMAND ) <<< "\$STDIN" 1>"\$STDOUT" 2>"\$STDERR"
 
 	command_result '$CONTENT_TYPE' "\$STDOUT" "\$STDERR" "\$DEBUG" "\$ERRORS" \\
-		"\$COMMAND" "\$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD"
+		"\$COMMAND" "\$FILTER_COMMAND" "\$QUERY_STRING" "\$REQUEST_METHOD" "\$STDIN"
 
 CGI
 